d414f9e1d2d5f1712f03d0df43214d164a4547ed
Virginia Lu <vl239@cornell.edu>
2022-06-16 16:52:24 -0400

    ef push2

---
+++ Infrastructure/Data/MovieShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+	public class MovieShopDbContext : DbContext
+	{
+		public MovieShopDbContext(DbContextOptions<MovieShopDbContext> options) : base(options)
+		{
+		}
+
+        // multiple DbSets
+
+        public DbSet<Genre> Genres { get; set; }
+
+
+    }
+}
+
