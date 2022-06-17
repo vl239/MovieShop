33fc9781009983546de907842436570d79fb4c92
Virginia Lu <vl239@cornell.edu>
2022-06-17 16:45:46 -0400

    Added responsive classes for movie card and tag helpers

--- MovieShopMVC/Controllers/MoviesController.cs
+++ MovieShopMVC/Controllers/MoviesController.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MovieShopMVC.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        // showing details of the movie
+        public IActionResult Details(int id)
         {
             return View();
         }
